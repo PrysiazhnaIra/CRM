1c009b64afe8cf404f937a2917159a1f37e56bf0
Ira Prysiazhna <ira.prisyazhna@gmail.com>
2025-02-06 15:14:57 +0200

    modules

--- crm/src/app/page.tsx
+++ crm/src/app/page.tsx
@@ -1,7 +1,12 @@
+import ActiveLabel from '@/app/components/active-label';
+import NotActiveLabel from '@/app/components/not-active-label';
+
 export default function Home() {
   return (
     <main>
       <h1>Home Page</h1>
+      <ActiveLabel>Active</ActiveLabel>
+      <NotActiveLabel>Not Active</NotActiveLabel>
     </main>
   );
 }
